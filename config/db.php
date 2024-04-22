81a1f5d27f9a6731268f53b8b7390c19891e7b90
chinhph28756 <chinhddph28756@fpt.edu.vn>
2024-04-22 14:33:44 +0700

    edit config/

--- config/db.php
+++ config/db.php
@@ -24,6 +24,7 @@ try {
     $connect = new PDO($queryDB1, $username, $password, $options);
     // sử dụng phương thức setAttribute để thiết lập chế độ xử lý lỗi
     $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Đã connect thành công";
 } catch (PDOException $e) {
     $message = $e->getMessage();
     echo "Lỗi kết nối cơ sở dữ liệu: " . $message;
