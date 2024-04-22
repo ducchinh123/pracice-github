20897b9c6127a361a29a9215fb0d90d6cea7643d
chinhph28756 <chinhddph28756@fpt.edu.vn>
2024-04-22 14:11:55 +0700

    add config/

---
+++ config/db.php
@@ -0,0 +1,31 @@
+<?php
+
+// Mysqli
+// $hostname = 'localhost';
+// $username = 'root';
+// $password = '';
+// $dbname = 'php_practice_1';
+
+// $connect = new mysqli($hostname, $username, $password, $dbname);
+
+// if($connect->connect_error) {
+//     echo "Lỗi kết nối: ".$connect->connect_error;
+// }
+
+// echo "Kết nối thành công";
+
+// PDO
+$queryDB1 = 'mysql:host=localhost;dbname=php_practice_1';
+$username = 'root';
+$password = '';
+$options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+
+try {
+    $connect = new PDO($queryDB1, $username, $password, $options);
+    // sử dụng phương thức setAttribute để thiết lập chế độ xử lý lỗi
+    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    $message = $e->getMessage();
+    echo "Lỗi kết nối cơ sở dữ liệu: " . $message;
+    exit();
+}
